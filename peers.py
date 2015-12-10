754622cbfb52384b892de723f4b321156f40795f
Matt Bruzek <matthew.bruzek@canonical.com>
2015-12-10 17:04:53 -0600

    Updating the logic with trial and error.

--- peers.py
+++ peers.py
@@ -12,50 +12,55 @@ class TlsPeer(RelationBase):
     individual units.'''
     scope = scopes.UNIT
 
-    @hook('{peers:certificates}-relation-joined')
+    @hook('{peers:tls}-relation-joined')
     def joined(self):
-        ''' '''
+        '''When peers join set the create certificate signing request state.'''
+        # Get the conversation scoped to the unit name.
         conv = self.conversation()
-        # start state
+        # Set the start state here for the layers to handle the logic.
         conv.set_state('create certificate signing request')
 
-
-    def get_units_in_state(self):
-        return [conv.scope for conv in self.conversations()]
-
-
     def set_csr(self, csr):
+        '''Set the certificate signing request (CSR) on the relation data.'''
         # Get the conversation scoped to the unit name.
-        # We are setting the CSR for this unit's conversation only.
         conv = self.conversation()
+        # Normaly we do not get the conversation this way, but for the peer
+        # relation we want to set the value for this unit's conversation only.
+
         # Remove the old state.
         conv.remove_state('create certificate signing request')
+        # Set the value on the relation.
         conv.set_remote({'csr': csr})
 
-
-    def get_csr_map():
-        '''Return a map of name and csr for each unit requesting a csr.
+    def get_csr_map(self):
+        '''Return a map of name and csr for each unit requesting a signed cert.
         Encapsulate the communication for all the units in this peer relation.
         '''
         csr_map = {}
-        converstations = self.conversations()
+        # Get all conversations of this type.
+        conversations = self.conversations()
+        # For each converation get the name and csr put them in a map.
         for conv in conversations:
             name = conv.scope
             csr = conv.get_remote('csr')
             csr_map[name] = csr
         return csr_map
 
-
-    def set_cert(name, certificate):
-        conv = self.conversation(name)
+    def set_cert(self, unit_name, certificate):
+        '''Set the signed server certificate on the relation data.'''
+        # Get the conversation scoped to the specific unit name.
+        conv = self.conversation(unit_name)
+        # Remove the sign
         conv.remove_state('sign certificate signing request')
         conv.set_remote({'signed_certificate': certificate})
 
-
-    @hook('{peers:certificates}-relation-changed')
+    @hook('{peers:tls}-relation-changed')
     def changed(self):
         '''Only the leader should change the state to sign the request. '''
+        # Get the conversation scoped to the unit name.
         conv = self.conversation()
+        # Normaly we do not get the conversation this way, but for the peer
+        # relation we want to get the value for this unit's conversation only.
         if is_leader():
             if conv.get_remote('csr'):
                 conv.set_state('sign certificate signing request')
@@ -64,12 +69,16 @@ class TlsPeer(RelationBase):
                 conv.set_state('signed certificate available')
 
     def get_signed_cert(self):
-        ''' '''
+        '''Return the signed certificate from the relation data.'''
+        # Get the conversation scoped to the unit name.
         conv = self.conversation()
+        # Normaly we do not get the conversation this way, but for the peer
+        # relation we want to get the value for this unit's conversation only.
         return conv.get_remote('signed_certificate')
 
-    @hook('{peers:certificates}-relation-{broken,departed}')
+    @hook('{peers:tls}-relation-{broken,departed}')
     def broken(self):
+        '''Can peer relations even be broken?'''
         unit = hookenv.remote_unit()
         print(unit)
         print('{relation_name}.available')
@@ -77,16 +86,3 @@ class TlsPeer(RelationBase):
         print(state)
         # Get the unit name here and remove that state.
         self.remove_state(state)
-
-    def configure(self):
-        # Generate the certs here?
-        print('configure')
-        unit = hookenv.remote_unit()
-        public_address = hookenv.relation_get('public-address')
-        private_address = hookenv.relation_get('private-address')
-        relation_info = {
-            '{0}_certificate'.format(unit): 'this certificate is for {0},{1}'.format(public_address, private_address),
-            '{0}_key'.format(unit): 'this key for {0},{1}'.format(public_address, private_address),
-        }
-
-        self.set_remote(**relation_info)
